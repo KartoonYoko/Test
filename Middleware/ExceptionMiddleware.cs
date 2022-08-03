fc0998667526b41dce97fb5d3d879785210810ff
developer2 <crashed9001work@gmail.com>
2022-08-03 10:32:37 +0900

    ..

---
+++ Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Middleware;
+
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        this._next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try {
+            await _next.Invoke(context);
+        }
+        catch(Exception e) {
+            await context.Response.WriteAsync(e.Message);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using WebApplication1.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/check", async (CheckHttp service, HttpContext ctx) => {
+    await ctx.Response.WriteAsync(await service.SendMessage("http://localhost:7010"));
+});
+
 
 app.Run();
