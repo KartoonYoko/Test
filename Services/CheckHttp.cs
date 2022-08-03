ce07c3db0dc419a8f7160240c82ff981c735050e
developer2 <crashed9001work@gmail.com>
2022-08-03 10:18:19 +0900

    ...

---
+++ Services/CheckHttp.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Services
+{
+    public class CheckHttp
+    {
+        private static HttpClient _httpClient;
+        static CheckHttp() {
+            _httpClient = new();
+        }
+
+        public async Task<string> SendMessage(string url) {
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
